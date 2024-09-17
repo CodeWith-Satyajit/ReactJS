c87b52bae89e20cebc6e4a15e9aade98c726f5dd
Narkhede Satyajit Nandkishor (SDS/ECE-CA2) <kcs3kor@bosch.com>
2024-09-18 02:48:56 +0530

    Ex04

--- App.js
+++ App.js
@@ -3,8 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const rootElement = document.getElementById('root');
-const element = React.createElement('h1', { id: 'my-heading' }, 'Hello, world!');
-console.log(element);
+// const element = React.createElement('h1', { id: 'my-heading' }, 'Hello, world!');
+// console.log(element);
+
+
+const HeadingComponent = () => {
+  return (<div>
+   <h1>React Component</h1> </div>)
+}
+
+
 
 const root = ReactDOM.createRoot(rootElement);
-root.render(element);
+root.render(<HeadingComponent />);
