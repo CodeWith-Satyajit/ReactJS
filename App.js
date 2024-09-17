71fac1665891fd0d68d0d180160e164063eee1e4
Narkhede Satyajit Nandkishor (SDS/ECE-CA2) <kcs3kor@bosch.com>
2024-09-17 15:33:59 +0530

    Ex02

---
+++ App.js
@@ -0,0 +1,7 @@
+
+const rootElement = document.getElementById('root');
+const element = React.createElement('h1', { id: 'my-heading' }, 'Hello, world!');
+console.log(element);
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(element);
