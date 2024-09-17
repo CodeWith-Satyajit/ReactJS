bb937e578bbfed4fa03ea16d47bd6269ccd2b669
Narkhede Satyajit Nandkishor (SDS/ECE-CA2) <kcs3kor@bosch.com>
2024-09-18 00:24:44 +0530

    Ex03

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const rootElement = document.getElementById('root');
 const element = React.createElement('h1', { id: 'my-heading' }, 'Hello, world!');
 console.log(element);
